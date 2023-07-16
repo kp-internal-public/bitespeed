a4a32b9dbb1b66915d0b180520fa03c5561aafe1
KaustubhPatange <developerkp16@gmail.com>
2023-07-16 10:47:22 +0530

    Rename endpoint to /identify

--- src/routes/identity.ts
+++ src/routes/identity.ts
@@ -1,26 +1,29 @@
-import IdentityController from "../controllers/identityController";
+import IdentityController from "../controllers/identifyController";
 import { FastifyInstance, FastifyPluginOptions } from "fastify";
-import z from "../@types/zod"
+import z from "../@types/zod";
 
 const bodySchema = z.object({
-  email: z.string().regex(/[\w\d\.]+@[\w\d]+\.[\w]{2,4}/g).optional(),
-  phoneNumber: z.string().optional()
-})
+  email: z
+    .string()
+    .regex(/[\w\d\.]+@[\w\d]+\.[\w]{2,4}/g)
+    .optional(),
+  phoneNumber: z.string().optional(),
+});
 
-type BodySchema = z.infer<typeof bodySchema>
+type BodySchema = z.infer<typeof bodySchema>;
 
 export default (
   fastify: FastifyInstance,
   options: FastifyPluginOptions,
-  done: Function
+  done: Function,
 ) => {
   fastify.post<{ Body: BodySchema }>(
-    "/identity",
+    "/identify",
     { schema: { body: z.toJsonSchema(bodySchema) }, ...options },
     async (request, reply) => {
-      const { email, phoneNumber } = request.body
+      const { email, phoneNumber } = request.body;
       return IdentityController.get().trackOrder(reply, email, phoneNumber);
-    }
+    },
   );
   done();
 };
